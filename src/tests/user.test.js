5469b8838a77aad1a1c3ab690f3679ea34078976
Jamie Catto <jamietcatto@gmail.com>
2023-02-27 17:14:15 +0100

    Complete CRUD testing for /users endpoints

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -22,4 +22,90 @@ describe('/users', () => {
           });
         });
       });
+
+    describe('with records in the databvase', () => {
+      let users;
+
+      beforeEach(async () => {
+        users = await Promise.all([
+          User.create({ username: "JamieCatto", email: "jamietcatto@gmail.com" }),
+          User.create({ username: "JoshBamford", email: "joshbam@gmail.com" }),
+          User.create({ username: "TestUser123", email: "testuser@gmail.com" }),
+        ]);
+      });
+
+      describe('GET /users', () => {
+        it('gets all users in the database', async () => {
+          const response = await request(app).get('/users');
+          
+          expect(response.status).to.equal(200);
+          expect(response.body.length).to.equal(3);
+          response.body.forEach((user) => {
+            const expected = users.find((a) => a.id === user.id);
+
+            expect(user.username).to.equal(expected.username);
+            expect(user.email).to.equal(expected.email);
+          });
+        });
+      });
+
+      describe('GET /users/:id', () => {
+        it('gets a user by their id', async () => {
+          const user = users[0];
+          const response = await request(app).get(`/users/${user.id}`);
+
+          expect(response.status).to.equal(200);
+          expect(response.body.username).to.equal(user.username);
+          expect(response.body.email).to.equal(user.email);
+        });
+
+        it('returns a 404 if their is no user with that id', async () => {
+          const response = await request(app).get('/users/9999999');
+
+          expect(response.status).to.equal(404);
+          expect(response.body.message).to.equal('user 9999999 does not exist.');
+        });
+      });
+
+      describe('PATCH /users/:id', () => {
+        it('updates a user\'s record by their id', async () => {
+          const user = users[0];
+          const response = await request(app).patch(`/users/${user.id}`).send({
+            username: "JamieCatto8"
+          });
+
+          expect(response.status).to.equal(200);
+          expect(response.body.id).to.equal(user.id);
+          expect(response.body.email).to.equal(user.email);
+          expect(response.body.username).to.equal('JamieCatto8');
+        });
+
+        it('returns a 404 if there is no user with that id', async () => {
+          const response = await request(app).patch('/users/9999999').send({
+            username: "JamieCatto8"
+          });
+
+          expect(response.status).to.equal(404);
+          expect(response.body.message).to.equal('user 9999999 does not exist.');
+        });
+      });
+
+      describe('DELETE /users/:id', () => {
+        it('deletes a user by id', async () => {
+          const user = users[0];
+          const response = await request(app).delete(`/users/${user.id}`);
+
+          const deletedUser = await User.findByPk(user.id, { raw: true });
+          expect(response.status).to.equal(204);
+          expect(deletedUser).to.equal(null);
+        });
+
+        it('returns a 404 if there is no user with that id', async () => {
+          const response = await request(app).delete('/users/9999999');
+
+          expect(response.status).to.equal(404);
+          expect(response.body.message).to.equal('user 9999999 does not exist.');
+        });
+      })
+    });
 });
