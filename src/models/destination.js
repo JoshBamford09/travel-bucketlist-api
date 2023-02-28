a21bb3cf6747a1394fede729e86ae4a94dd208fc
Jamie Catto <jamietcatto@gmail.com>
2023-02-28 08:37:39 +0100

    Added validation and 'country' field to Destination model

--- src/models/destination.js
+++ src/models/destination.js
@@ -1,7 +1,36 @@
 module.exports = (connection, DataTypes) => {
     const schema = {
-        name: DataTypes.STRING,
-        description: DataTypes.STRING, 
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    args: true,
+                    msg: 'You must enter a name for the destination.'
+                },
+                notEmpty: {
+                    args: true,
+                    msg: 'You cannot leave the destination name empty.'
+                }
+            }
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    args: true,
+                    msg: 'You must enter a description for the destination.'
+                },
+                notEmpty: {
+                    args: true,
+                    msg: 'You cannot leave the description empty.'
+                }
+            }
+        },
+        country: {
+            type: DataTypes.STRING
+        }
     };
 
     const DestinationModel = connection.define('Destination', schema);
